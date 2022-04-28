b451ff54b360399417c5dbd1725ed712dde6c2ca
dwsantos <66379964+dwsantos@users.noreply.github.com>
2022-04-27 17:22:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -132,25 +132,3 @@ let x = Math.floor((Math.random() * 100) + 1);
 rname = names[x];
 alert(rname);
 }
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
