661ec389f80ba9ae481ea7ed4c95eb1552ec485a
dwsantos <dwsantos1016@gmail.com>
2022-06-17 00:01:34 -0700

    CAP

--- games/xg.js
+++ games/xg.js
@@ -13,15 +13,15 @@ button(){
 **/
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+var counter=0;
 function jump(){
-    if (character.classList != "animate"){
-        character.classList.add("animate");
-    }
-        setTimeout(function(){
-            character.classList.remove("animate");
-        }, 500);
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
 }
-var checkDead = setInterval(function(){
+var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
@@ -33,4 +33,4 @@ var checkDead = setInterval(function(){
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
-        }, 10);;
+}, 10);
