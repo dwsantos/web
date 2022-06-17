571b01d10077cbc67353565f4cff8d753e2b4197
dwsantos <66379964+dwsantos@users.noreply.github.com>
2022-06-16 21:47:36 -0700

    Update xg.js

--- games/xg.js
+++ games/xg.js
@@ -1,1 +1,14 @@
+/**var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 1080;
+        this.canvas.height = 720;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+
+button(){
+    myGameArea.start();
+}
+**/
 
