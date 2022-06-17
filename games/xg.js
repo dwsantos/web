8ba77c9901fcd4d1e14d0f1aa8c5a9f2c1e80c35
dwsantos <66379964+dwsantos@users.noreply.github.com>
2022-06-16 21:59:21 -0700

    Update xg.js

--- games/xg.js
+++ games/xg.js
@@ -11,4 +11,13 @@ button(){
     myGameArea.start();
 }
 **/
-
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+function jump(){
+    if (character.classList != "animate"){
+        character.classList.add("animate");
+    }
+        setTimeout(function(){
+            character.classList.remove("animate");
+        }, 500);
+}
